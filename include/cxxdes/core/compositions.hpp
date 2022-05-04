70e4194d125b809c884bc037a078e4cc35b64678
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2022-05-04 18:05:16 +0200

    Use iterators instead of vectors.

--- include/cxxdes/core/compositions.hpp
+++ include/cxxdes/core/compositions.hpp
@@ -12,7 +12,6 @@
 #define CXXDES_CORE_COMPOSITIONS_HPP_INCLUDED
 
 #include <tuple>
-#include <vector>
 #include <iterator>
 
 #include <cxxdes/core/process.hpp>
@@ -75,25 +74,26 @@ struct giant1 {
         }
     };
 
-    template <awaitable A>
+    template <typename It>
     struct result_type_runtime {
-        std::vector<A> awaitables;
+        It first, last;
+        std::size_t size;
 
         event *on_suspend(promise_base *promise, coro_handle coro) {
             auto handler = new new_handler;
             handler->done = false;
-            handler->remaining = awaitables.size();
+            handler->remaining = size;
             handler->output_event = new event{0, 0, coro};
             handler->env = promise->env;
-            for (auto &awaitable: awaitables)
-                awaitable.on_suspend(promise, coro)->handler = handler;
+            for (auto it = first; it != last; ++it)
+                it->on_suspend(promise, coro)->handler = handler;
             return handler->output_event;
         }
 
         void on_resume() {
             // call on_resume on each awaitable
-            for (auto &awaitable: awaitables)
-                awaitable.on_resume();
+            for (auto it = first; it != last; ++it)
+                it->on_resume();
         }
     };
 
@@ -107,8 +107,7 @@ struct giant1 {
         template <typename It>
         [[nodiscard("expected usage: co_await any_of.sequence(begin, end) or all_of.sequence(begin, end)")]]
         constexpr auto sequence(It first, It last) const {
-            using value_type = typename std::iterator_traits<It>::value_type;
-            return result_type_runtime<value_type>{std::vector<value_type>(first, last)};
+            return result_type_runtime<It>{first, last, (std::size_t) std::distance(first, last)};
         }
     };
 
@@ -160,28 +159,27 @@ struct giant2 {
         }
     };
 
-    template <awaitable A>
+    template <typename It>
     struct result_type_runtime {
-        std::vector<A> awaitables;
+        It first, last;
 
-        static process<> p(environment *env, std::vector<A> &awaitables) {
-            for (auto &awaitable: awaitables) {
-                co_await awaitable;
+        static process<> p(environment *env, It first, It last) {
+            for (auto it = first; it != last; ++it) {
+                co_await *it;
             }
-            co_return ;
         }
         
         event *on_suspend(promise_base *promise, coro_handle coro) {
             event *output_event = new event{ 0, 1000, coro };
-            auto pp = p(promise->env, awaitables);
+            auto pp = p(promise->env, first, last);
             pp.this_promise()->completion_evt = output_event;
             pp.start(*(promise->env));
             return output_event;
         }
 
         void on_resume() {
-            for (auto &awaitable: awaitables) {
-                awaitable.on_resume();
+            for (auto it = first; it != last; ++it) {
+                it->on_resume();
             }
         }
     };
@@ -196,8 +194,7 @@ struct giant2 {
         template <typename It>
         [[nodiscard("expected usage: co_await sequential.sequence(begin, end)")]]
         constexpr auto sequence(It first, It last) const {
-            using value_type = typename std::iterator_traits<It>::value_type;
-            return result_type_runtime<value_type>{std::vector<value_type>(first, last)};
+            return result_type_runtime<It>{first, last};
         }
     };
 };
