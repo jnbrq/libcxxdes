f6f50ee5cb616fb7d76ef8d3d2367bf4a72618bf
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2022-08-22 10:13:27 +0200

    More tests.

--- tests/controlflow.test.cpp
+++ tests/controlflow.test.cpp
@@ -53,3 +53,43 @@ TEST(ControlFlowTest, Async2) {
 
     test{}.run();
 }
+
+TEST(ControlFlowTest, Compositions1) {
+    CXXDES_SIMULATION(test) {
+        process<void> co_main() {
+            time_type expected_now = 0;
+
+            co_await all_of(timeout(10), timeout(20));
+            expected_now += 20;
+            EXPECT_EQ(now(), expected_now);
+
+            co_await any_of(timeout(10), timeout(20));
+            expected_now += 10;
+            EXPECT_EQ(now(), expected_now);
+
+            co_await ((delay(1000) && delay(5)) || (delay(100) && delay(1)));
+            expected_now += 100;
+            EXPECT_EQ(now(), expected_now);
+
+            co_await (delay(10), delay(20), delay(30));
+            expected_now += 60;
+            EXPECT_EQ(now(), expected_now);
+
+            {
+                auto a = (delay(1000) && delay(5)) || (delay(100) && delay(1));
+                co_await a;
+                expected_now += 100;
+                EXPECT_EQ(now(), expected_now);
+            }
+
+            {
+                auto a = (delay(10), delay(20), delay(30));
+                co_await a;
+                expected_now += 60;
+                EXPECT_EQ(now(), expected_now);
+            }
+        }
+    };
+
+    test{}.run();
+}
